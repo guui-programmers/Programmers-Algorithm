fe341ef29cf3b9bd3e85f493b0a01c217975feb2
simchodi <simchodi@gmail.com>
2022-06-14 19:57:55 +0900

    자연수뒤집어배열로만들기 / lv.1 / 3분
    https://programmers.co.kr/learn/courses/30/lessons/12932

---
+++ simchodi/자연수뒤집어배열로만들기.java
@@ -0,0 +1,12 @@
+class Solution {
+    public int[] solution(long n) {
+        int[] answer = new int[(n + "").length()];
+        // int 배열에 하나씩 저장
+        int index = 0;
+        for(int i = ((n + "").length())-1; i >= 0; i--){
+            answer[index++] = Integer.parseInt((n + "").substring(i,i+1));
+        }
+        return answer;
+    }
+}
+
