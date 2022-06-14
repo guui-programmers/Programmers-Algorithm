014aa371f4b4cf4245bc50b097634baf4fd5e4ce
HyunDel <pshyun1331@gmail.com>
2022-06-14 20:16:40 +0900

    최대공약수와 최소공배수/lv.1

---
+++ 상현 알고리즘/최대공약수와 최소공배수.py
@@ -0,0 +1,13 @@
+def solution(n, m):
+    answer=[]
+    for i in range(max(n,m),0,-1):
+        if n % i ==0 and m % i ==0 :
+            answer.append(i)
+            break;
+    
+    for k in range(max(n,m),(n*m)+1):
+        if k%n ==0 and k%m ==0 :
+            answer.append(k)
+            break;
+        
+    return answer
