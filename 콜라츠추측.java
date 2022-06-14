9acf769256a957e7b7465d58563a9a2e694cf2b9
simchodi <simchodi@gmail.com>
2022-06-14 17:12:22 +0900

    콜라츠 추측/ lv.1 / 30분
    https://programmers.co.kr/learn/courses/30/lessons/12943

---
+++ 콜라츠추측.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int solution(int num) {
+        int count = 0;
+        long num2 = (long)num;
+        while (num2 != 1){
+            if (count == 499){
+                break;
+            }
+            if (num2 % 2 == 0){
+                num2 = num2 / 2;
+            }else{
+                num2 = (num2 * 3) + 1;
+            }
+            count++;
+        }
+        return count == 499 ? -1 : count;
+    }
+}
