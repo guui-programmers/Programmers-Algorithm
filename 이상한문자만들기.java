bc898a3f3069ad493f4bb58a103240014c8cd254
simchodi <simchodi@gmail.com>
2022-06-14 17:25:09 +0900

    이상한문자만들기 / lv.1 / 20분
    https://programmers.co.kr/learn/courses/30/lessons/12930

---
+++ 이상한문자만들기.java
@@ -0,0 +1,22 @@
+class Solution {
+    public String solution(String s) {
+        String answer = "";
+        s = s.toLowerCase();
+        char[] s_char = s.toCharArray();
+        int index = 0;
+        int inner_index = 0;
+        
+        while(index < s_char.length){
+            if(s_char[index] == ' '){
+                inner_index = -1;
+            }
+            if(inner_index % 2 == 0){
+                s_char[index] = Character.toUpperCase(s_char[index]);
+            }
+            answer += s_char[index];
+            index++;
+            inner_index++;
+        }
+        return answer;
+    }
+}
